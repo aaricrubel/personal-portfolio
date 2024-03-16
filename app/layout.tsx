369192994edd0e83b907f9ab142c7451825338cb
akashkmt <akashkmt963@gmail.com>
2024-03-17 02:52:18 +0530

    initail commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">Side Nav</div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
